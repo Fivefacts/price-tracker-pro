9b85f918b34684ad5a0fb769b8c577e32af9ce66
Fivefact <fivefactsfr@gmail.com>
2025-12-02 10:59:42 +0100

    Migrate to PostgreSQL

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from email_service import EmailService
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pricetracker.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///pricetracker.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True,
+    'pool_recycle': 300,
+}
 
 db.init_app(app)
 login_manager = LoginManager()
